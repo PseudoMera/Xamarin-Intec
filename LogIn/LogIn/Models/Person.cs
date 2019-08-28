ccc59cc5f62d3bf7fdde9df41643c8859cccd6cd
Albin Frias <albinesteban@outlook.com>
2019-08-28 17:50:13 -0400

    Added MVVM and Transitions

---
+++ LogIn/LogIn/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogIn.Models
+{
+    class Person
+    {
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+
+    
+    }
+}
---
+++ LogIn/LogIn/ViewModel/PersonPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using LogIn.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace LogIn.ViewModel
+{
+    class PersonPageViewModel
+    {
+        public ICommand LoginCommand { get; set; }
+        public Person MyPerson { get; set; } = new Person();
+        public PersonPageViewModel()
+        {
+            LoginCommand = new Command(async () =>
+            {
+                
+                if (string.IsNullOrEmpty(MyPerson.UserName))
+                {
+
+                    await App.Current.MainPage.DisplayAlert("Error", "You have to enter a valid username", "Ok");
+                }
+                else if (string.IsNullOrEmpty(MyPerson.PassWord))
+                {
+                    await App.Current.MainPage.DisplayAlert("Error", "You have to enter a valid password", "Ok");
+                }
+                else
+                {
+                    await App.Current.MainPage.DisplayAlert("Welcome", $"Hello {MyPerson.UserName}", "Ok");
+                }
+            });
+           
+        }
+    }
+}
