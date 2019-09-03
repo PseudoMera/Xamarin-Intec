072df5b6665fdd2c60f908492b720944c931306f
Albin Frias <albinesteban@outlook.com>
2019-09-03 00:20:39 -0400

    Added taking photos functionality

--- ContactsManager/ContactsManager/ViewModels/AddContactPageViewModel.cs
+++ ContactsManager/ContactsManager/ViewModels/AddContactPageViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using ContactsManager.Models;
 using ContactsManager.Views;
+using Plugin.Media;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,14 +13,44 @@ namespace ContactsManager.ViewModels
     {
         public Contact contact { get; set; } = new Contact();
         public ICommand AddCommand { get; set; }
+        public ICommand TakePhotoClicked { get; set; }
         public AddContactPageViewModel()
         {
+            TakePhotoClicked = new Command(async () =>
+            {
+                await CrossMedia.Current.Initialize();
+
+                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+                {
+                    await App.Current.MainPage.DisplayAlert("No Camera", ":( No camera available.", "OK");
+                    return;
+                }
+
+                var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+                    Directory = "Sample",
+                    Name = "test.jpg"
+                });
+
+                if (file == null)
+                    return;
+
+                await App.Current.MainPage.DisplayAlert("File Location", file.Path, "OK");
+
+                contact.Photo = file.Path;
+            });
+
             AddCommand = new Command(async () =>
             {
                 MessagingCenter.Send(this, "CreateContact", contact);
                 await App.Current.MainPage.Navigation.PopAsync();
                 //await App.Current.MainPage.Navigation.PushAsync(new ContactsListPage());
             });
+
+          
+
         }
+
+      
     }
 }
--- ContactsManager/ContactsManager/Views/AddContactPage.xaml.cs
+++ ContactsManager/ContactsManager/Views/AddContactPage.xaml.cs
@@ -1,11 +1,17 @@
 ﻿using ContactsManager.ViewModels;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
 using Xamarin.Forms.Xaml;
 
 namespace ContactsManager.Views
@@ -13,10 +19,17 @@ namespace ContactsManager.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AddContactPage : ContentPage
     {
+
+       
         public AddContactPage()
         {
             InitializeComponent();
+            
+
+
             this.BindingContext = new AddContactPageViewModel();
         }
+
+    
     }
 }
